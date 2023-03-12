491a1ce0f09ee93d4f6cc606bceacf59dac5d905
yjson <handyejee14@gmail.com>
2023-03-12 23:32:29 +0900

    [선택정렬] 함수명 변경

--- selection_sort.py
+++ selection_sort.py
@@ -1,18 +1,21 @@
-def findSmallest(arr):
-    smallest = arr[0]
-    smallest_index = 0
+# 선택정렬
+def findSmallListIndex(arr): 
+    smallest = arr[0] # 가장 작은 정수를 저장
+    smallest_index = 0 # 가장 작은 정수의 인덱스를 저장
     
-    for i in range(1, len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_index = i
+    for j in range(1, len(arr)):
+        if arr[j] < smallest:
+            smallest = arr[j]
+            smallest_index = j
+
     return smallest_index
 
 def selectionSort(arr):
-    newArr = []
+    new_array = []
     for i in range(len(arr)):
-        smallest = findSmallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
+        smallest = findSmallListIndex(arr)
+        new_array.append(arr.pop(smallest))
+        
+    return new_array
 
 print (selectionSort([5, 3, 6, 2, 10]))
